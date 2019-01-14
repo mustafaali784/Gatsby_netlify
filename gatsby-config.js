3abe7d0ae4574ce14fe8ee84c89d3ef81ad21d21
mustafaali784 <mustafaali786687@gmail.com>
2019-01-14 16:42:39 +0530

    disabled create pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,20 +21,21 @@ module.exports = {
         path: `${__dirname}/src/pages`,
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `WP-App`,
-        short_name: `WP`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `standalone`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `WP-App`,
+    //     short_name: `WP`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `standalone`,
+    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    'gatsby-plugin-offline',
+    // 'gatsby-plugin-offline',
   ],
 }
